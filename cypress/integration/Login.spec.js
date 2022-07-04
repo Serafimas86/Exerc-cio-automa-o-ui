863088bb276539b0ebb660ba9d7b3f2c9a1f1a08
aline <aline.serafim86@gmail.com>
2022-07-03 21:27:02 -0300

    exercicio de automacao ui - modulo 11

--- cypress/integration/Login.spec.js
+++ cypress/integration/Login.spec.js
@@ -2,8 +2,16 @@
 
 context('Funcionalidade Login', () =>{
 
-    it('Deve fazer login com suceso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+    
+    it('Deve fazer login com suceso', () =>{
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -13,7 +21,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuario inválido', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('janete@teste.com')
         cy.get('#password').type('g1@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -23,7 +30,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('g1@teste.com')
         cy.get('.woocommerce-form > .button').click()
---
+++ cypress/integration/Produtos.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('[50806209] – Produto Lgc2')
+            .click()
+        
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        cy.get('[class="product-block grid"]')
+        .contains('[50806209] – Produto Lgc2').click()
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 1)
+    
+        cy.get('.woocommerce-message').should('contain', '“[50806209] – Produto Lgc2” foi adicionado no seu carrinho.')
+    });
+
+});
