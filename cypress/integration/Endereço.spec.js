6954599870d61f34438ed0ee7dc2727661b88f51
aline <aline.serafim86@gmail.com>
2022-07-21 14:25:27 -0300

    Usando arquivo de dado e comando customizado

--- cypress/integration/Endereço.spec.js
+++ cypress/integration/Endereço.spec.js
@@ -1,17 +1,48 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados =>{
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
-        
-    });
-    
-    it('Deve fazer cadastro de faturameto com sucesso', () => {
-        
-        //cadastro de endereço
 
     });
+
+    it('Deve fazer cadastro de faturameto com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Aline', 'Serafim', 'Alto Relevo', 'Brasil', 'João José Guimarães', '963', 'Sombrio', 'Santa Catarina', '88960000', '48996138771', 'aline.serafim86@gmail.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturameto com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            
+            
+            
+            
+            
+            
+            
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
 });
+
+
+
+
+
